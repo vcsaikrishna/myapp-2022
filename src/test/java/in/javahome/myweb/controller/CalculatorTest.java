920a57529ed6bee816b28acb4b98ebda8ec8b91b
vcsaikrishna <46190879+vcsaikrishna@users.noreply.github.com>
2022-11-07 22:36:20 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -46,4 +46,12 @@ public class CalculatorTest extends TestCase {
 
 		}
 	}
+
+	public void testMultiply() {
+		Assert.assertEquals(cal.multiply(10, 20), 200);
+	}
+	
+	public void testAddNew() {
+		Assert.assertEquals(cal.add(10, 20), 30);
+	}
 }
