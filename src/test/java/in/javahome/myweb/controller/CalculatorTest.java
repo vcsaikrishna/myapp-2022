8ad91b13c1f60d0f2aa7ba73062e421c51f7a99c
vcsaikrishna <46190879+vcsaikrishna@users.noreply.github.com>
2022-11-07 22:38:33 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Properties;
 
-import junit.framework.Assert;
+
 import junit.framework.TestCase;
 
 public class CalculatorTest extends TestCase {
@@ -47,11 +47,4 @@ public class CalculatorTest extends TestCase {
 		}
 	}
 
-	public void testMultiply() {
-		Assert.assertEquals(cal.multiply(10, 20), 200);
-	}
-	
-	public void testAddNew() {
-		Assert.assertEquals(cal.add(10, 20), 30);
-	}
 }
